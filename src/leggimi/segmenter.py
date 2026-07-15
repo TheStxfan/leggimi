36ff861992d9b42d497fce9353070103d035f188
TheStxfan <stefan.ferro5@gmail.com>
2026-07-15 14:03:35 +0200

    feat: add segmenter stub

---
+++ src/leggimi/segmenter.py
@@ -0,0 +1,15 @@
+def split_chapters(pages: list[Page]) -> list[Chapter]:
+    """
+    Divide le pagine in capitoli basandosi sui titoli.
+
+    Args:
+        pages: Lista di oggetti Page.
+
+    Returns:
+        Lista di oggetti Chapter.
+
+    Raises:
+        ChaptersNotFoundError: se i capitoli non esistono.
+        NotImplementedError: implementazione non ancora disponibile.
+    """
+    raise NotImplementedError
