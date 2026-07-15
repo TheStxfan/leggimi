ae98da386468ba537d5f38a8ab32bb9e3e698e85
TheStxfan <stefan.ferro5@gmail.com>
2026-07-15 16:17:04 +0200

    test: add FileNotFoundError to test_extractor.py

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -6,3 +6,8 @@ from leggimi.extractor import extract_text
 def test_extract_text_esiste():
     with pytest.raises(NotImplementedError):
         extract_text(str(Path("qualsiasi.pdf")))
+
+
+def test_extract_text_file_non_esistente():
+    with pytest.raises(FileNotFoundError):
+        extract_text(str(Path("qualsiasi.pdf")))
