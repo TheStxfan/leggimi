5d6948c082aea85bbc9e495d9c8865792cf57ba3
TheStxfan <stefan.ferro5@gmail.com>
2026-07-31 18:18:34 +0200

    refactor: remove unused ModelNotFoundError import in config

--- src/leggimi/config.py
+++ src/leggimi/config.py
@@ -2,7 +2,7 @@ import os
 from typing import Literal
 
 from dotenv import load_dotenv
-from leggimi.errors import LLMNonDisponibileError, ModelNotFoundError
+from leggimi.errors import LLMNonDisponibileError
 
 load_dotenv()
 
